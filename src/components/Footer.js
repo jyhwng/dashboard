860556dabadeb3fab8a350726627e1e37bdd3e16
Jiyoung Hwang <jy.hwng@gmail.com>
2017-10-28 23:28:43 +0900

    Add fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,11 +8,25 @@ class Footer extends Component {
             <Segment className="fixed-footer-container">
                 <Grid className="fixed-footer">
                     <Grid.Column width={8}>
-                        test1
+                        <h1>CHECK OUT PRESELECTED DEMOS</h1>
+                        <Grid>
+                            <Grid.Column width={8}>Vertical Menu (Default)</Grid.Column>
+                            <Grid.Column width={8}>Horizontal Menu + Boxed Container</Grid.Column>
+                        </Grid>
                     </Grid.Column>
                     <Grid.Column width={8}>
-                        test2
+                        <h1>CHECK OUT PRESELECTED DEMOS</h1>
+                        <Grid.Row columns={2}>
+                            <Grid.Column>
+                            </Grid.Column>
+                            <Grid.Column>
+                            Clean UI – a modern professional admin template, based on Bootstrap 4 framework. Clean UI is a powerful and super flexible tool, which suits best for any kind of web application: Web Applications; CRM; CMS; Admin Panels; Dashboards; etc. Clean UI is fully responsive, which means that it looks perfect on mobiles and tablets
+                            </Grid.Column>
+                        </Grid.Row>
                     </Grid.Column>
+                    <Grid.Row columns={1}>
+                        <Grid.Column>© 2016 Mediatec Software All rights reserved</Grid.Column>
+                    </Grid.Row>
                 </Grid>
             </Segment>
         )
