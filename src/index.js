769b7fc558b1f7d22e2a55949021cf544c30af06
Jiyoung Hwang <jiyoung@repick.co>
2017-11-21 19:17:45 +0900

    Implement code-splitting with AsyncComponent

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-
+// import AuthenticatedRoute from "./components/AuthenticatedRoute";
+// import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 import './index.css';
 import App from './App';
-import Dashboard from './containers/Dashboard';
-import Form from './components/Form/Form';
+import asyncComponent from './components/AsyncComponent';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
+
+const AsyncDashboard = asyncComponent(() => import('./containers/Dashboard'));
+const AsyncForm = asyncComponent(() => import('./components/Form/Form'));
 
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-      <IndexRoute component={Dashboard} />
-      <Route path="dashboard" component={Dashboard} />
-      <Route path="form" component={Form} />
+      <IndexRoute exact component={AsyncDashboard} />
+      <Route path="dashboard" exact component={AsyncDashboard} />
+      <Route path="form" exact component={AsyncForm} />
     </Route>
   </Router>,
   document.getElementById('root')
