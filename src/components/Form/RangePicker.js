37d5069300fb9f1e6e503aa60d1c96286f3f0652
Jiyoung Hwang <jiyoung@repick.co>
2017-11-29 15:28:59 +0900

    Use styled-component for tooltip

--- src/components/Form/RangePicker.js
+++ src/components/Form/RangePicker.js
@@ -2,18 +2,17 @@ import React, { Component } from 'react';
 import MainContainer from '../../containers/MainContainer';
 import { Segment, Grid, Divider } from 'semantic-ui-react';
 import './RangePicker.css';
-import $ from 'jquery';
-import { findDOMNode } from 'react-dom';
 import Rheostat from 'rheostat';
+import styled from 'styled-components'
 // https://github.com/airbnb/rheostat
 
 class RangePicker extends Component {
-  handleDrag = () => {
-    const handle = findDOMNode(this.refs.handle)
-    console.log(handle)
-    $(handle).draggable({ axis: "x" });
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: 0
+    }
   }
-
   render() {
     let dataPercent = 56;
     const rangePickerStyle = {
@@ -26,8 +25,58 @@ class RangePicker extends Component {
       left: dataPercent.toString() + '%'
     };
     const tooltipStyle = {
-      left: dataPercent.toString() + '%'
+      position: 'absolute',
+      left: this.state.value.toString() + '%'
     };
+    let updateValue = (value) => {
+      this.setState({value:value.values})
+    }
+    // let rheostatHandleStyle = {
+    //   '--content': dataPercent.toString(),
+    // }
+    let rheostatHandleStyle = {
+      position: 'absolute',
+      left: this.state.value + '%',
+    }
+    let rheostatBarStyle = {
+      position: 'absolute',
+      width: this.state.value + '%',
+    }
+
+    const RheostatTooltip = styled.div`
+      left: ${this.state.value}%;
+      position: absolute;
+      background-color: #01a8fe;
+      width: 24px;
+      margin-left: -12px;
+      height: 16.1px;
+      border-radius: 3px;
+      top: -29px;        
+      font-size: 10px;
+      font-weight: 600;
+      text-align: center;
+      color: #ffffff;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    `
+
+    let customHandle = () => {
+      return (
+        <div>
+          <RheostatTooltip>{this.state.value}</RheostatTooltip>
+          <div className="rheostat-handle" 
+            style={rheostatHandleStyle} 
+            aria-valuemax="100" aria-valuemin="0" aria-valuenow={this.state.value} 
+            aria-disabled="false" data-handle-key="0" class="rheostat-handle" 
+            role="slider" tabindex="0" type="button">
+          </div>
+        </div>
+      )
+    }
+    let customProgressBar = () => {
+      return <div className="rheostat-progress" style={rheostatBarStyle}></div>
+    }
     // const scale = [0, ... , 100]
     // https://developer.mozilla.org/en-US/docs/Web/Events/dragstart
     return (
@@ -43,7 +92,7 @@ class RangePicker extends Component {
                     data-percent={dataPercent}
                     style={barStyle}
                   />
-                  <div className="handle" onDragStart={()=>this.handleDrag()} ref="handle" style={handleStyle} />
+                  <div className="handle" style={handleStyle} />
                   <div className="tooltip start">0</div>
                   <div className="tooltip" style={tooltipStyle}>
                     {dataPercent}
@@ -71,10 +120,19 @@ class RangePicker extends Component {
             <Grid.Column>
               <Segment>
               <Rheostat
-                min={1}
+                min={0}
+                max={100}
+                values={[this.state.value]}
+                handle={customHandle}
+                progressBar={customProgressBar}
+                onValuesUpdated={updateValue}
+              />
+              <Divider hidden/>
+              <Rheostat
+                style={rheostatHandleStyle}
+                min={0}
                 max={100}
                 values={[56]}
-                pitPoints={10}
               />
               </Segment>
             </Grid.Column>
